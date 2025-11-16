dcce35ef2ee68163f7d8bbd2b4b1a45d8658ebe5
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-16 16:19:00 +0100

    Rename mpfit to fmpfit

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ ext_modules = [
 
 setup(
     name="ftools",
-    version="4.0.18",
+    version="4.0.19",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
