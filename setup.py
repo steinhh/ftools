80f0d9ab2dba88e8a393d2d23cc1bab80b6c188f
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-17 21:20:39 +0100

    Commit scratch-instructions/callsign.txt before handing off to Claude

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ ext_modules = [
 
 setup(
     name="ftools",
-    version="4.0.28",
+    version="4.0.29",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
