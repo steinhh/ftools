cd61a77a5d9191453521a44d2e9e5e783f70df24
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-24 11:43:27 +0100

    Add example_fmpfit_5_N.py for 5-pixel case

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ ext_modules = [
 
 setup(
     name="ftools",
-    version="4.0.38",
+    version="4.0.39",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
