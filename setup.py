afe0511b5222b319fbe8b63aba6f9529ca3ff030
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-26 13:16:06 +0100

    Move fmpfit-initial-instructions.txt to fmpfit directory

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ ext_modules = [
 
 setup(
     name="ftools",
-    version="4.0.68",
+    version="4.0.70",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
