7b960ef69e5f3b71e6cba96c0d7bfa7f17c389e4
steinhh <s.v.h.haugan@astro.uio.no>
2026-01-09 19:55:36 +0100

    Add description of version 6 change (ftools -> ftoolss)

--- setup.py
+++ setup.py
@@ -145,7 +145,8 @@ setup(
     name="ftoolss",
     # Version 5 introduced fmpfit
     # Version 5.2 introduces xerror_scipy (different handling of bounded params)
-    version="6.0.22",
+    # Version 6 changed name to ftoolss (ftools, Stein's) for publishing to PiPY
+    version="6.0.23",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
